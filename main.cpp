a318211624307ea7b33cc47c386c97bd598a3095
DESOUKI <desouki.infosec@gmail.com>
2026-06-05 23:48:46 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -19,8 +19,17 @@ private:
 
 public:
   graduateStudent(string n, int i, string tT, double g) : Student(n, i), thesisTitle(tT), gpa(g) {}
+  void display()
+  {
+    cout << "Name: " << name << endl
+         << "ID: " << id << endl
+         << "Thesis title: " << thesisTitle << "GPA: " << gpa << endl;
+  }
 };
 
 int main()
 {
+  graduateStudent gs("DESOUKI", 1001, "CS", 3.92);
+  gs.display();
+  return 0;
 }
