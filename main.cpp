de825b65126ed528a5b9873ccfd16510895f229c
DESOUKI <desouki.infosec@gmail.com>
2026-06-05 23:36:06 +0300

    newTask

--- main.cpp
+++ main.cpp
@@ -1,51 +1,14 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-class Person
+class Student
 {
-private:
-  string name;
-  int age;
-
-public:
-  Person() : name("Unknown"), age(0) {}
-  Person(string n, int a) : name(n), age(a) {}
-  string getName()
-  {
-    return name;
-  }
-  int getAge()
-  {
-    return age;
-  }
 };
 
-class Employee : public Person
+class graduateStudent : public Student
 {
-private:
-  double salary;
-  string jobTitle;
-
-public:
-  Employee() : Person()
-  {
-    salary = 0.0;
-    jobTitle = "Unknown";
-  }
-  Employee(string n, int a, string jT, double s) : Person(n, a), salary(s), jobTitle(jT) {}
-  void display()
-  {
-    cout << "Name: " << getName() << endl
-         << "Age: " << getAge() << endl
-         << "Salary: $" << salary << endl
-         << "Job title: " << jobTitle << endl;
-  }
 };
 
 int main()
 {
-  Employee emp("DESOUKI", 20, "AI Engineer", 12000);
-  emp.display();
-  return 0;
 }
