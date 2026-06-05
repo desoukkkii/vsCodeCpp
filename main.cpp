3ebc2b47ab750b8f579b495c1867bf1e735a98d1
DESOUKI <desouki.infosec@gmail.com>
2026-06-05 23:55:26 +0300

    Added constructors

--- main.cpp
+++ main.cpp
@@ -3,10 +3,15 @@ using namespace std;
 
 class bankAccount
 {
+private:
+  int accountNumber;
+  double balance;
 };
 
 class savingAccount : public bankAccount
 {
+private:
+  double interestRate;
 };
 
 int main()
