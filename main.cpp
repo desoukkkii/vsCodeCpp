5f0991b1064333ca66975b24113ed49f03b08a83
DESOUKI <desouki.infosec@gmail.com>
2026-06-06 00:05:30 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -6,14 +6,40 @@ class bankAccount
 private:
   int accountNumber;
   double balance;
+
+public:
+  bankAccount(int aN, double bal) : accountNumber(aN), balance(bal) {}
+  int getAccN()
+  {
+    return accountNumber;
+  }
+  double getBal()
+  {
+    return balance;
+  }
 };
 
 class savingAccount : public bankAccount
 {
 private:
   double interestRate;
+
+public:
+  savingAccount(int aN, double bal, double intR) : bankAccount(aN, bal), interestRate(intR) {}
+  double intRate()
+  {
+    return getBal() + (getBal() * interestRate);
+  }
+  void display()
+  {
+    cout << "Account number: " << getAccN() << endl
+         << "Balance: " << getBal() << endl
+         << intRate() << endl;
+  }
 };
 
 int main()
 {
+  savingAccount sv(1, 1000, 1);
+  sv.display();
 }
