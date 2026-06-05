495ef6e2d26e3e2aafc367517dacac099ef409cd
DESOUKI <desouki.infosec@gmail.com>
2026-06-05 23:53:19 +0300

    New task

--- main.cpp
+++ main.cpp
@@ -1,35 +1,14 @@
 #include <iostream>
 using namespace std;
 
-class Student
+class bankAccount
 {
-protected:
-  string name;
-  int id;
-
-public:
-  Student(string n, int i) : name(n), id(i) {}
 };
 
-class graduateStudent : public Student
+class savingAccount : public bankAccount
 {
-private:
-  string thesisTitle;
-  double gpa;
-
-public:
-  graduateStudent(string n, int i, string tT, double g) : Student(n, i), thesisTitle(tT), gpa(g) {}
-  void display()
-  {
-    cout << "Name: " << name << endl
-         << "ID: " << id << endl
-         << "Thesis title: " << thesisTitle << "GPA: " << gpa << endl;
-  }
 };
 
 int main()
 {
-  graduateStudent gs("DESOUKI", 1001, "CS", 3.92);
-  gs.display();
-  return 0;
 }
