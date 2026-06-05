29bbe8df5e003ba49df3ddeabc10010565b20e01
DESOUKI <desouki.infosec@gmail.com>
2026-06-05 23:14:48 +0300

    Added constructors

--- main.cpp
+++ main.cpp
@@ -3,9 +3,13 @@ using namespace std;
 
 class Person
 {
-private:
+protected:
   string name;
   int age;
+
+public:
+  Person() : name("Unknown"), age(0) {}
+  Person(string n, int a) : name(n), age(a) {}
 };
 
 class Employee : public Person
@@ -13,6 +17,16 @@ class Employee : public Person
 public:
   double salary;
   string jobTitle;
+  Employee() : Person()
+  {
+    salary = 0.0;
+    jobTitle = "Unknown";
+  }
+  Employee(double s, string jT, string n, int a) : Person(n, a)
+  {
+    salary = s;
+    jobTitle = jT;
+  }
 };
 
 int main()
