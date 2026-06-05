1f3a759602a0123205887c6301aa6dd5e16fac36
DESOUKI <desouki.infosec@gmail.com>
2026-06-06 00:33:33 +0300

    New task

--- main.cpp
+++ main.cpp
@@ -1,36 +1,14 @@
 #include <iostream>
 using namespace std;
 
-class Person
+class Item
 {
-protected:
-  string name;
-  int id;
-
-public:
-  Person(string n, int i) : name(n), id(i) {}
 };
 
-class Doctor : public Person
+class Bool : public Item
 {
-private:
-  string specialization;
-  double salary;
-
-public:
-  Doctor(string n, int i, string spec, double sal) : Person(n, i), specialization(spec), salary(sal) {}
-  void dis()
-  {
-    cout << "Name: " << name << endl
-         << "ID: " << id << endl
-         << "Specialization: " << specialization << endl
-         << "Salary: " << salary << endl;
-  }
 };
 
 int main()
 {
-  Doctor doc("MOHAMMED", 202, "CS", 22000);
-  doc.dis();
-  return 0;
 }
