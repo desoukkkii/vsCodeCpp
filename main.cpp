b9e4659c4f56e9e502fc8afee99c40fa9b6e1ade
DESOUKI <desouki.infosec@gmail.com>
2026-06-05 23:43:27 +0300

    Added constructors

--- main.cpp
+++ main.cpp
@@ -3,10 +3,22 @@ using namespace std;
 
 class Student
 {
+protected:
+  string name;
+  int id;
+
+public:
+  Student(string n, int i) : name(n), id(i) {}
 };
 
 class graduateStudent : public Student
 {
+private:
+  string thesisTitle;
+  double gpa;
+
+public:
+  graduateStudent(string n, int i, string tT, double g) : Student(n, i), thesisTitle(tT), gpa(g) {}
 };
 
 int main()
