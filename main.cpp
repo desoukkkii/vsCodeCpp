064120cba276692ec5811b21c350ff2d9b6ca83d
DESOUKI <desouki.infosec@gmail.com>
2026-06-06 16:12:09 +0300

    Area of Rectangle

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class Rectangle
+{
+private:
+  double height, width;
+
+public:
+  Rectangle(double h, double w) : height(h), width(w) {}
+  double getArea() { return height * width; }
+};
+
 int main()
 {
+  Rectangle r(5, 5);
+  cout << "Area: " << r.getArea() << endl;
+  return 0;
 }
