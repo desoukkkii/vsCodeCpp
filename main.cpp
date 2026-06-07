d1a18ac4842f6ed860fad734e9013571fd8b27dd
DESOUKI <desouki.infosec@gmail.com>
2026-06-07 04:29:54 +0300

    DONE

--- main.cpp
+++ main.cpp
@@ -1,26 +1,40 @@
 #include <iostream>
 using namespace std;
 
-class Student
+class Person
 {
 protected:
   int id;
   string name;
 
 public:
-  Student(int i, string n) : id(i), name(n) {}
-
-  void dis()
-  {
-    cout << "ID: " << id << endl
-         << "Name: " << name << endl;
-  }
+  Person(int i, string n) : id(i), name(n) {}
+  ~Person() { cout << "Done!"; }
 };
+
+class Student : public Person
+{
+private:
+  double grade, gpa;
+
+public:
+  Student(int i, string n, double gr, double gp) : Person(i, n), grade(gr), gpa(gp) {}
+
+  void dis() { cout << "Display function called!"; }
+
+  friend void disInfo(Student s);
+};
+
+void disInfo(Student s) { cout << "ID: " << s.id << endl
+                               << "Name: " << s.name << endl
+                               << "Grade: " << s.grade << endl
+                               << "GPA: " << s.gpa << endl; }
 
 int main()
 {
-  Student s(1, "DESOUKI");
+  Student s(1, "DESOUKI", 97.00, 4.00);
   Student *ptr = &s;
   ptr->dis();
+  disInfo(s);
   return 0;
 }
