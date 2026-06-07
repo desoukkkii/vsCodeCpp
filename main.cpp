ccf4308aa4eb7857728f47ee2f3272d63cd6010b
DESOUKI <desouki.infosec@gmail.com>
2026-06-07 03:32:39 +0300

    New

--- main.cpp
+++ main.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 using namespace std;
 
-class Rectangle
+class Person
 {
-private:
-  double height, width;
+protected:
+  int id;
+  string name;
 
 public:
-  Rectangle(double h, double w) : height(h), width(w) {}
-  double getArea() { return height * width; }
+  Person(int i, string n) : id(i), name(n) {}
+  ~Person() { cout << "Person Destructore Called."; }
 };
+
+class Student : public Person
+{
+private:
+  double grade;
+  string department;
+
+public:
+  Student(int i, string n, double g, string d) : Person(i, n), grade(g), department(d) {}
+
+  void dis()
+  {
+    cout << "ID: " << id << endl
+         << "Name: " << name << endl
+         << "Grade: " << grade << endl
+         << "Department: " << department << endl;
+  }
+
+  friend void grade(Student s);
+};
+
+void grade(Student s) { cout << "Grade: " << s.grade; }
 
 int main()
 {
-  Rectangle r(5, 5);
-  cout << "Area: " << r.getArea() << endl;
+  Student s1(1, "Student 1", 4.00, "CS"), s2(2, "Student 2", 3.00, "AI");
+  s1.dis();
+  grade(s1);
+  cout << endl;
+  s2.dis();
+  grade(s2);
   return 0;
 }
