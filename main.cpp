5633f73d6428c496001c61724389884dd4e64e24
DESOUKI <desouki.infosec@gmail.com>
2026-06-07 03:45:27 +0300

    DONE

--- main.cpp
+++ main.cpp
@@ -1,46 +1,39 @@
 #include <iostream>
 using namespace std;
 
-class Person
+class Exam
 {
 protected:
-  int id;
-  string name;
+  double midMark, finalMark;
 
 public:
-  Person(int i, string n) : id(i), name(n) {}
-  ~Person() { cout << "Person Destructore Called."; }
+  Exam(double mK, double fK) : midMark(mK), finalMark(fK) {}
 };
 
-class Student : public Person
+class Sports
+{
+protected:
+  double sportsMark;
+
+public:
+  Sports(double sK) : sportsMark(sK) {}
+};
+
+class Result : public Exam, public Sports
 {
 private:
-  double grade;
-  string department;
+  double totalMark;
 
 public:
-  Student(int i, string n, double g, string d) : Person(i, n), grade(g), department(d) {}
-
-  void dis()
-  {
-    cout << "ID: " << id << endl
-         << "Name: " << name << endl
-         << "Grade: " << grade << endl
-         << "Department: " << department << endl;
-  }
-
-  friend void grade(Student s);
+  Result(double mK, double fK, double sK) : Exam(mK, fK), Sports(sK), totalMark(0) {}
+  double calculateTotal() { return midMark + finalMark + sportsMark; }
+  void dis() { cout << calculateTotal(); }
 };
-
-void grade(Student s) { cout << "Grade: " << s.grade; }
 
 int main()
 {
-  Student s1(1, "Student 1", 4.00, "CS"), s2(2, "Student 2", 3.00, "AI");
-  s1.dis();
-  grade(s1);
-  cout << endl;
-  s2.dis();
-  grade(s2);
+  Result r(1.00, 2.00, 3.00);
+  r.calculateTotal();
+  r.dis();
   return 0;
 }
