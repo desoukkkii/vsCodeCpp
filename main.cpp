4a1865a7f1a92239d7d4b68a43e4df871a6fd4e9
Mohammed Desouki <desouki.infosec@gmail.com>
2026-06-14 23:47:00 +0300

    null

--- main.cpp
+++ main.cpp
@@ -1,31 +1,7 @@
 #include <iostream>
 using namespace std;
-
-class Person
-{
-public:
-  virtual void display() { cout << "Person."; }
-};
-
-class Student : public Person
-{
-private:
-  int id;
-
-public:
-  Student(int i) : id(i) {}
-  Student(const Student &s) : id(s.id) {}
-
-  void display() { cout << "ID: " << id << endl; }
-};
-
 int main()
 {
-  Student s1(1001), s2(s1);
-  Student *ptr1 = &s2;
-  Person *ptr2 = &s1;
-  ptr1->display();
-  ptr2->display();
-
-  return 0;
+	cout << "Hello, world!";
+	return 0;
 }
